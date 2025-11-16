40bfb39243840570531f4580436b28cfa5e61456
fadlullan <fadlullahherifarid@gmail.com>
2025-11-16 16:37:05 +0800

    add main.php

---
+++ main.php
@@ -0,0 +1,58 @@
+<div class="overflow-hidden bg-white py-24 sm:py-32 dark:bg-gray-900">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <p class="max-w-2xl text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-6xl sm:text-balance dark:text-white">Teori & Simulasi</p>
+        <div class="relative mt-16 h-144 sm:h-auto">
+            <div class="absolute -inset-2 rounded-[calc(var(--radius-xl)+calc(var(--spacing)*2))] shadow-xs ring-1 ring-black/5 dark:bg-white/2.5 dark:ring-white/10"></div>
+
+            <div class="min-h-full">
+                <div class="relative bg-gray-800 pb-32 rounded-[calc(var(--radius-xl)+calc(var(--spacing)*2))]">
+                    <nav class="bg-gray-800 rounded-[calc(var(--radius-xl)+calc(var(--spacing)*2))]">
+                        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                            <div class="border-b border-white/10">
+                                <div class="flex h-16 items-center justify-between px-4 sm:px-0">
+                                    <div class="flex items-center">
+                                        <div class="shrink-0">
+                                            <img src="img/logo.png" alt="Logo SPKPedia" class="size-8" />
+                                        </div>
+                                        <div class="ml-4 flex items-baseline space-x-4">
+                                            <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
+                                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Kriteria</a>
+                                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Penilaian</a>
+                                            <a href="#" class="hidden md:block rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Hasil</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="flex items-center justify-between px-4 sm:px-0 md:hidden mb-2">
+                                    <div class="flex items-center">
+                                        <div class="mr-8"></div>
+                                        <div class="ml-4 flex items-baseline space-x-4">
+                                            <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Hasil</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+
+                <main class="relative -mt-28">
+                    <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+                        <div class="rounded-lg bg-white px-5 py-6 shadow-sm sm:px-6">
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                            <br>ulla
+                        </div>
+                    </div>
+                </main>
+            </div>
+
+        </div>
+    </div>
+</div>
