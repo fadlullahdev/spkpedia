e4d472a57d0a45ff1f6e163878a1a7a5b3950fd5
fadlullan <fadlullahherifarid@gmail.com>
2025-11-17 22:52:42 +0800

    update landingpage navbar

--- components/navbar.php
+++ components/navbar.php
@@ -13,7 +13,8 @@
 
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                     <a href="" class="inline-flex items-center border-b-2 border-indigo-600 px-1 pt-1 text-sm font-medium text-gray-900 dark:border-indigo-500 dark:text-white">Home</a>
-                    <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white">About</a>
+                    <a href="about" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white">About</a>
+                    <a href="articles" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white">Articles</a>
                 </div>
             </div>
 
@@ -63,7 +64,8 @@
     <div id="mobile-menu" class="hidden sm:hidden opacity-0 transition-opacity duration-200">
         <div class="space-y-1 pt-2 pb-3">
             <a href="" class="block border-l-4 border-indigo-600 bg-indigo-50 py-2 pr-4 pl-3 text-base font-medium text-indigo-700 dark:border-indigo-500 dark:bg-indigo-600/10 dark:text-indigo-400">Home</a>
-            <a href="#" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white">About</a>
+            <a href="about" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white">About</a>
+            <a href="articles" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white">Articles</a>
         </div>
         <div class="border-t border-gray-200 pt-4 pb-3 dark:border-white/10">
             <div class="flex justify-around items-center px-4">
